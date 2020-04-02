8d7effa42b43c9e84af71b1bf793d90149dfead2
Andrey <zhegalovan@mail.ru>
2020-04-02 21:47:57 +0300

    added class HelloOtus

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,3 +1,4 @@
 public interface Versions {
     String guava = "28.2-jre";
+    String junit = "4.12";
 }
---
+++ hw01-gradle/src/main/java/hw01/gradle/HelloOtus.java
@@ -0,0 +1,8 @@
+package hw01.gradle;
+
+class HelloOtus{
+
+    boolean someMethod(){
+        return true;
+    }
+}
