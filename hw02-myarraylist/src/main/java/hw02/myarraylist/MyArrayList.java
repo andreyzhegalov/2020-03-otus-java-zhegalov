ac9317f7dd68ef98bbe003019afa0377a6d71de3
Andrey <zhegalovan@mail.ru>
2020-04-12 13:06:17 +0300

    added all tests

--- hw02-myarraylist/src/main/java/hw02/myarraylist/MyArrayList.java
+++ hw02-myarraylist/src/main/java/hw02/myarraylist/MyArrayList.java
@@ -14,132 +14,112 @@ class MyArrayList<T>  implements List<T>{
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public <T> T[] toArray(T[] a) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean add(T e) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends T> c) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean addAll(int index, Collection<? extends T> c) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public T set(int index, T element) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void add(int index, T element) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public T remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public ListIterator<T> listIterator() {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public ListIterator<T> listIterator(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public List<T> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 }
 
--- hw02-myarraylist/src/test/java/hw02/myarraylist/MyArrayListTest.java
+++ hw02-myarraylist/src/test/java/hw02/myarraylist/MyArrayListTest.java
@@ -1,13 +1,164 @@
 package hw02.myarraylist;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.junit.jupiter.api.Assertions;
 
 public class MyArrayListTest {
 	@Test
 	public void testSize() {
-		Assertions.assertThrows(UnsupportedOperationException.class, ()->{
-			new MyArrayList<Integer>().size();
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().size();
+		});
+	}
+
+	@Test
+	public void testIsEmpty() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().isEmpty();
+		});
+	}
+
+	@Test
+	public void testContains() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().contains(null);
+		});
+	}
+
+	@Test
+	public void testIterator() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().iterator();
+		});
+	}
+
+	@Test
+	public void testToArray() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().toArray();
+		});
+	}
+
+	@Test
+	public void testToArrayFromArray() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().toArray(new Object[10]);
+		});
+	}
+
+	@Test
+	public void testAdd() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().add(new Object());
+		});
+	}
+
+	@Test
+	public void testRemove() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().remove(new Object());
+		});
+	}
+
+	@Test
+	public void testContainsAll() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().containsAll(new ArrayList<Object>());
+		});
+	}
+
+	@Test
+	public void testAddAll() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().addAll(1, new ArrayList<Object>());
+		});
+	}
+
+	@Test
+	public void testRemoveAll() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().removeAll(new ArrayList<Object>());
+		});
+	}
+
+	@Test
+	public void testRetainAll() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().retainAll(new ArrayList<Object>());
+		});
+	}
+
+	@Test
+	public void testClear() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().clear();
+		});
+	}
+
+	@Test
+	public void testGet() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().get(0);
+		});
+	}
+
+	@Test
+	public void testSet() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().set(0, new Object());
+		});
+	}
+
+	@Test
+	public void testAddByIndex() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().add(0, new Object());
+		});
+	}
+
+	@Test
+	public void testRemoveByIndex() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().remove(0);
+		});
+	}
+
+	@Test
+	public void testIdexOf() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().indexOf(new Object());
+		});
+	}
+
+	@Test
+	public void testLastIndexOf() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().indexOf(new Object());
+		});
+	}
+
+	@Test
+	public void testListIterator() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().listIterator();
+		});
+	}
+
+	@Test
+	public void testListIteratorByIndex() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().listIterator(0);
+		});
+	}
+
+	@Test
+	public void testSubList() {
+		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+			new MyArrayList<Object>().subList(0, 0);
 		});
 	}
 }
