bd95291a687b2a9cd754ec88591897e293630b56
Andrey <zhegalovan@mail.ru>
2020-05-28 14:37:14 -0400

    added toJson() for null and empty objects

---
+++ hw08-json-object-writer/src/main/java/hw08/mygson/MyGson.java
@@ -0,0 +1,25 @@
+package hw08.mygson;
+
+import java.lang.reflect.Field;
+
+public class MyGson {
+
+    public String toJson(Object obj) throws IllegalArgumentException, IllegalAccessException {
+        if (obj == null) {
+            return "null";
+        }
+
+        final var clazz = obj.getClass();
+        final Field[] fields = clazz.getDeclaredFields();
+        for (int i = 0; i < fields.length; i++) {
+            final var field = fields[i];
+            field.setAccessible(true);
+            System.out.println( field.getType().getTypeName());
+            System.out.println(field.getName());
+            System.out.println(field.getBoolean(obj));
+        }
+
+        return "{}";
+    }
+
+}
---
+++ hw08-json-object-writer/src/test/java/hw08/mygson/MyGsonTest.java
@@ -0,0 +1,38 @@
+package hw08.mygson;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.gson.Gson;
+
+import org.junit.jupiter.api.Test;
+
+public class MyGsonTest {
+
+    @Test
+    public void testToJsonNull() throws IllegalArgumentException, IllegalAccessException {
+        assertEquals(new Gson().toJson(null), new MyGson().toJson(null));
+    }
+
+    class Empty {
+    }
+
+    @Test
+    public void testToJsonEmptyClass() throws IllegalArgumentException, IllegalAccessException {
+        final var obj = new Empty();
+        assertEquals(new Gson().toJson(obj), new MyGson().toJson(obj));
+    }
+
+    class WithBoolean {
+        private boolean b;
+
+        public WithBoolean(boolean b) {
+            this.b = b;
+        }
+    }
+
+    @Test
+    public void testToJsonForBoolean() throws IllegalArgumentException, IllegalAccessException {
+        final var objWithTrue = new WithBoolean(true);
+        assertEquals(new Gson().toJson(objWithTrue), new MyGson().toJson(objWithTrue));
+    }
+}
