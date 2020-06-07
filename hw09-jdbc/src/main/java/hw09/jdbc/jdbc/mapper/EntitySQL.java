122d626c2ced7d181df3bd3eb119f320ce1fe548
Andrey <zhegalovan@mail.ru>
2020-06-07 04:29:52 -0400

    added EntitySQL

--- hw09-jdbc/src/main/java/hw09/jdbc/jdbc/mapper/EntitySQL.java
+++ hw09-jdbc/src/main/java/hw09/jdbc/jdbc/mapper/EntitySQL.java
@@ -1,26 +1,52 @@
 package hw09.jdbc.jdbc.mapper;
 
-public class EntitySQL implements EntitySQLMetaData{
+import com.google.common.base.CaseFormat;
 
-	@Override
-	public String getSelectAllSql() {
+public class EntitySQL<T> implements EntitySQLMetaData {
+    final EntityClassMetaData<T> entityClass;
+
+    public EntitySQL(EntityClassMetaData<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    @Override
+    public String getSelectAllSql() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("select ");
+
+        final var fields = entityClass.getAllFields();
+        if (fields.isEmpty()) {
+            throw new RuntimeException("Can't create sql request for type " + entityClass.getName());
+        }
+        final var iter = fields.iterator();
+        while (iter.hasNext()) {
+            sb.append(toLowerUnderScore( iter.next().getName()));
+            if (iter.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        sb.append(" from ");
+        sb.append(toLowerUnderScore(entityClass.getName()));
+        return sb.toString();
+    }
+
+    @Override
+    public String getSelectByIdSql() {
         throw new UnsupportedOperationException();
-	}
+    }
 
-	@Override
-	public String getSelectByIdSql() {
+    @Override
+    public String getInsertSql() {
         throw new UnsupportedOperationException();
-	}
+    }
 
-	@Override
-	public String getInsertSql() {
+    @Override
+    public String getUpdateSql() {
         throw new UnsupportedOperationException();
-	}
+    }
 
-	@Override
-	public String getUpdateSql() {
-        throw new UnsupportedOperationException();
-	}
-
+    private String toLowerUnderScore(String in){
+        final var res = new String();
+        return CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, in);
+    }
 }
-
--- hw09-jdbc/src/test/java/hw09/jdbc/jdbc/mapper/EntitySQLTest.java
+++ hw09-jdbc/src/test/java/hw09/jdbc/jdbc/mapper/EntitySQLTest.java
@@ -1,6 +1,7 @@
 package hw09.jdbc.jdbc.mapper;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -8,26 +9,45 @@ import org.junit.jupiter.api.Test;
 public class EntitySQLTest {
     @Test
     public void testCtr() {
-        assertDoesNotThrow(() -> new EntitySQL());
+        assertDoesNotThrow(() -> new EntitySQL<Object>(null));
+    }
+
+    @Test
+    public void testGetSelectAllSqlForEmptyType() {
+        class Test {
+        }
+        assertThrows(RuntimeException.class,
+                () -> new EntitySQL<Test>(new EntityClass<Test>(Test.class)).getSelectAllSql());
     }
 
     @Test
     public void testGetSelectAllSql() {
-        assertThrows(UnsupportedOperationException.class, ()->new EntitySQL().getSelectAllSql());
+        class ClassForTesting {
+            final int id = 0;
+            final String strField = new String();
+        }
+        assertEquals("select id, str_field from class_for_testing",
+                new EntitySQL<ClassForTesting>(new EntityClass<ClassForTesting>(ClassForTesting.class)).getSelectAllSql());
     }
 
     @Test
     public void testGetSelectByIdSql() {
-        assertThrows(UnsupportedOperationException.class, ()->new EntitySQL().getSelectByIdSql());
+        assertThrows(UnsupportedOperationException.class, () -> new EntitySQL<Object>(null).getSelectByIdSql());
     }
 
     @Test
     public void testGetInsertSql() {
-        assertThrows(UnsupportedOperationException.class, ()-> new EntitySQL().getInsertSql());
+        class ClassForTesting {
+            final int id = 0;
+            final String strField = new String();
+            final int intFiled = 1;
+        }
+        assertEquals("insert into classfortesting(name) values (?)", new EntitySQL<ClassForTesting>(new EntityClass<ClassForTesting>(ClassForTesting.class)) );
     }
 
     @Test
     public void testGetUpdateSql() {
-        assertThrows(UnsupportedOperationException.class, ()->new EntitySQL().getUpdateSql());
+        assertThrows(UnsupportedOperationException.class, () -> new EntitySQL<Object>(null).getUpdateSql());
     }
+
 }
