3eee7af2348446bcd1f34fef4667a8dc55e89943
Andrey <zhegalovan@mail.ru>
2020-07-28 16:00:30 -0400

    added sync condition

--- hw15-executors/src/main/java/hw15/ShiftedSequence.java
+++ hw15-executors/src/main/java/hw15/ShiftedSequence.java
@@ -1,29 +1,36 @@
 package hw15;
 
-import org.slf4j.LoggerFactory;
-
-import hw15.generator.TriangleGenerator;
-
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import hw15.generator.TriangleGenerator;
 
 public class ShiftedSequence {
     private static final Logger logger = LoggerFactory.getLogger(ShiftedSequence.class);
 
     private final Lock lock = new ReentrantLock();
+    private final Condition otherThread;
+    private String lastThreadName;
+
+    public ShiftedSequence() {
+        otherThread = lock.newCondition();
+    }
 
     public static void main(String[] args) throws InterruptedException {
         new ShiftedSequence().go();
     }
 
     private void go() throws InterruptedException {
-        Thread t1 = new Thread(()->criticalSection(1_000));
+
+        Thread t1 = new Thread(() -> criticalSection(1_000));
         t1.setName("t1");
         t1.start();
 
-        Thread t2 = new Thread(()->criticalSection(2_000));
+        Thread t2 = new Thread(() -> criticalSection(2_000));
         t2.setName("t2");
         t2.start();
 
@@ -33,18 +40,32 @@ public class ShiftedSequence {
 
     private void criticalSection(long sleepMs) {
         final var generator = new TriangleGenerator(1, 10);
+        final var currentThreadName = Thread.currentThread().getName();
         while (!Thread.currentThread().isInterrupted()) {
             lock.lock();
             try {
+                while (currentThreadName.equals(lastThreadName)) {
+                    try {
+                        otherThread.await();
+                    } catch (Exception e) {
+                        logger.error(e.getMessage());
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                lastThreadName = currentThreadName;
+                otherThread.signalAll();
+
                 final var nextValue = generator.getNext();
                 if (nextValue.isEmpty()) {
                     break;
+                } else {
+                    logger.info("new value {}", nextValue.get());
+                    sleep(sleepMs);
                 }
-                logger.info("new value {}", nextValue.get());
+
             } finally {
                 lock.unlock();
             }
-            sleep(sleepMs);
         }
     }
 
--- hw15-executors/src/main/java/hw15/generator/TriangleGenerator.java
+++ hw15-executors/src/main/java/hw15/generator/TriangleGenerator.java
@@ -5,26 +5,26 @@ import java.util.Optional;
 public class TriangleGenerator {
     private final int maxValue;
     private final int startValue;
-    private int currentVaue;
+    private int currentValue;
     private boolean isIncrease = true;
 
     public TriangleGenerator(int startValue, int maxValue) {
         this.startValue = startValue;
         this.maxValue = maxValue;
-        this.currentVaue = startValue;
+        this.currentValue = startValue;
     }
 
     public Optional<Integer> getNext() {
         if (startValue >= maxValue) {
             return Optional.empty();
         }
-        if (currentVaue < startValue) {
+        if (currentValue < startValue) {
             return Optional.empty();
         }
-        if (currentVaue == maxValue) {
+        if (currentValue == maxValue) {
             isIncrease = false;
         }
 
-        return isIncrease ? Optional.of(currentVaue++) : Optional.of(currentVaue--);
+        return isIncrease ? Optional.of(currentValue++) : Optional.of(currentValue--);
     }
 }
