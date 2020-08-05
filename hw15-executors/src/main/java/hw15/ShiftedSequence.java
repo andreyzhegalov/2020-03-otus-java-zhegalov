28185b495c1d03bfb4baa8c9b6ea3d0b0e09e178
Andrey <zhegalovan@mail.ru>
2020-08-05 13:35:58 -0400

    added countdownlatch

--- hw15-executors/src/main/java/hw15/ShiftedSequence.java
+++ hw15-executors/src/main/java/hw15/ShiftedSequence.java
@@ -1,5 +1,6 @@
 package hw15;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -13,6 +14,7 @@ public class ShiftedSequence {
     private static final Logger logger = LoggerFactory.getLogger(ShiftedSequence.class);
 
     private final Lock lock = new ReentrantLock();
+    private final CountDownLatch callingThreadBlocker = new CountDownLatch(1);
     private final Condition otherThread;
     private String lastThreadName;
 
@@ -26,26 +28,33 @@ public class ShiftedSequence {
 
     private void go() throws InterruptedException {
 
-        Thread t1 = new Thread(() -> criticalSection(1_000));
+        Thread t1 = new Thread(() -> criticalSection(1_000, true));
         t1.setName("t1");
 
-        Thread t2 = new Thread(() -> criticalSection(2_000));
+        Thread t2 = new Thread(() -> criticalSection(2_000, false));
         t2.setName("t2");
 
         t1.start();
-        t1.join(100);
-
         t2.start();
 
         t1.join();
         t2.join();
     }
 
-    private void criticalSection(long sleepMs) {
+    private void criticalSection(long sleepMs, boolean startFirst) {
         final var generator = new TriangleGenerator(1, 10);
         final var currentThreadName = Thread.currentThread().getName();
+        if (!startFirst) {
+            try {
+                callingThreadBlocker.await();
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage());
+                Thread.currentThread().interrupt();
+            }
+        }
         while (!Thread.currentThread().isInterrupted()) {
             lock.lock();
+            callingThreadBlocker.countDown();
             try {
                 while (currentThreadName.equals(lastThreadName)) {
                     try {
