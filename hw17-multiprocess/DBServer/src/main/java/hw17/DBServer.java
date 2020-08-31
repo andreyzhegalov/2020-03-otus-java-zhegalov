564209d6cf7854a1b5ddd9b083c737fda70b9a56
Andrey <zhegalovan@mail.ru>
2020-08-31 04:02:10 -0400

    minor fix

--- hw17-multiprocess/DBServer/src/main/java/hw17/DBServer.java
+++ hw17-multiprocess/DBServer/src/main/java/hw17/DBServer.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import hw17.config.AppConfig;
 import hw17.messageservice.MessageSystemClient;
 
-@ComponentScan({"hw17"})
+@ComponentScan("hw17")
 public class DBServer {
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
